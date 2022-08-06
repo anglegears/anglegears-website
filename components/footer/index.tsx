80619261bc814c2fd6340776ef3e78f4dcfe6274
StarfallV <envricochen10@gmail.com>
2022-08-06 09:02:56 +0700

    update main page

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Footer() { 
 
     return (
-        <footer className="flex h-24 w-full items-center border-t">
+        <footer className="flex h-24 w-full items-center text-white">
             <div className="flex items items-center mx-20 justify-start w-full">
                 <div className="inset-y-0 left-0 w-1/2">
                     <p>
-                        © 2022 Starsoft Studio | Pemerintah Provinsi Maluku. All Rights Reserved.
+                        © 2022 Starsoft Studio. All Rights Reserved.
                     </p>
                 </div>
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,55 +20,46 @@ const Home: NextPage = () => {
 
   return (
     <>
+      <div className="bg-violet-agr-900 text-white bg-[url('/assets/header.png')] w-screen h-screen absolute -z-50" />
+      {/* <div className="bg-[url('/carousel/image_1.png')] w-screen h-screen absolute -z-50 opacity-20" /> */}
+      <Head>
+        <title>Angle Gears | Racing Metaverse World</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Navbar login={setModalLoginOpen} />
       <div className="flex min-h-screen flex-col items-center justify-center py-2">
         <div onClick={BackdropClick} className={`absolute z-50 w-screen h-screen bg-black/50 backdrop-blur-sm justify-center items-center flex transition-all duration-500 ${modalLoginOpen ? 'visible opacity-100' : 'visibility-hidden opacity-0'}`}>
           <ModalLogin open={modalLoginOpen} onClose={() => setModalLoginOpen(false)} />
         </div>
-
-        <Head>
-          <title>Nikola</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
         <main className="flex w-full flex-1 flex-row justify-center">
-          <div className="flex w-1/2 flex-1 flex-col items-start justify-center px-20 text-gray-800 gap-7">
-            <div className="flex flex-row gap-4 items-center justify-center h-32">
-              <Image src="/logo_gov.png" alt="govmaluku logo" width={92} height={92} />
-              <h1 className="text-5xl font-bold">
-                Pemerintah Provinsi Maluku (Bangsat)
-              </h1>
-            </div>
+          <div className="flex w-1/2 flex-1 flex-col items-start justify-center px-20 text-white gap-7">
 
             <div className="flex flex-col items-baseline">
               <h1 className="text-6xl font-bold">
-                Klinik Konsultasi dan Layanan{' '}
+                Explore your creativity.
               </h1>
-              <h1 className="text-6xl font-bold text-blue-600">
-                ANJAB ABK
+              <h1 className="text-6xl font-bold text-sky-300">
+                Have fun with the community.
               </h1>
             </div>
 
             <div className="flex items-baseline space-x-6">
-              <button onClick={() => setModalLoginOpen(true)} className="bg-blue-700 hover:bg-blue-500 text-white font-semibold py-3 px-14 rounded-full transition ease-in-out dubration-300">
-                Masuk
+              <button onClick={() => setModalLoginOpen(true)} className="bg-transparent shadow-md shadow-sky-600 hover:shadow-sky-400 hover:scale-105 text-white font-semibold py-2 px-10 rounded-xl transition ease-in-out duration-300">
+                Explore
               </button>
-              <button className="bg-green-600 hover:bg-green-500 text-white font-semibold py-3 px-10 rounded-full transition ease-in-out duration-300">
-                Tentang kami
+              <button className="bg-transparent shadow-md shadow-fuchsia-400 hover:shadow-fuchsia-300 hover:scale-105 text-white py-2 px-10 rounded-xl transition ease-in-out duration-300">
+                Mint gCar NFT
               </button>
             </div>
 
             <div className="flex flex-col items-baseline">
               <h1 className="text-2xl font-bold">
-                #BanggaMelayaniBangsa{' '}
+                Welcome to the world of Angle Gears.
               </h1>
             </div>
           </div>
 
-          <div className="flex w-1/2 flex-1 flex-col items-start justify-center text-center text-gray-800 gap-7  mr-10">
-            <div className="bg-[url('/bg/bg_illustration.png')] bg-cover w-full h-3/5">
-            </div>
-          </div>
         </main>
 
         <Footer />
